1e2c60659852101843b3143bb8333fcdd3c25573
laypatel13 <laypatel1322008@gmail.com>
2026-04-08 17:30:20 +0530

    feature: add basic display window

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-# Checking some things.
+import time 
+import random
+import math
+import pygame
+pygame.init()
 
-print("aim-trainer")
+WIDTH, HEIGHT = 800, 600
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Aim-traineR")
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
